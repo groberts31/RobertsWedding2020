4bb796bf8e52017dc00df236a28255e6346024b6
Greg Roberts <groberts3104@gmail.com>
2021-01-11 15:22:39 -0500

    updated Navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,19 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function Navbar() {
   return (
-    <ul className="navList">
-      <li>
-        <a href="">Pictures</a>
-      </li>
-      <li>
-        <a href="">About Us</a>
-      </li>
-      <li>
-        <a href="">Videos</a>
-      </li>
-    </ul>
+    <span className="navigation">
+
+    <nav className="navbar navbar-expand-lg">
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/home"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/pictures"
+              className={window.location.pathname === "/pictures" ? "nav-link active" : "nav-link"}
+            >
+              Pictures
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/videos"
+              className={window.location.pathname === "/videos" ? "nav-link active" : "nav-link"}
+            >
+              Videos
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/about"
+              className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+            >
+              About Us
+            </Link>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+    </span>
   );
 }
 
